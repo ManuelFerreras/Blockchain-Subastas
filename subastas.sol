563c7c09fb2b9a17d62e7eaba574653cc91c6ccf
Manuel Ferreras <35044975+ManuelFerreras@users.noreply.github.com>
2021-12-06 19:17:02 -0300

    finished

--- subastas.sol
+++ subastas.sol
@@ -16,6 +16,7 @@ contract subastas_a_ciegas {
     }
 
     mapping(uint256 => subasta) subastas;
+    uint[] historialSubastas = new uint[](0);
 
     uint256 contadorSubastas = 0;
 
@@ -29,15 +30,19 @@ contract subastas_a_ciegas {
     event eventoSubastaOfertada(uint256, uint256, address); // Id, Precio, Direccion Ofertante
 
     
-    function crearSubasta(uint256 _precioSubasta, string memory _descripcionSubasta) public {
+    function crearSubasta(uint256 _precioSubasta, string memory _descripcionSubasta, uint _tiempoFinSubasta) public {
+
+        require(_precioSubasta > 0, "El precio inicial no puede ser 0.");
+        require(_tiempoFinSubasta > block.timestamp + 10 minutes, "Tiempo de Fin Invalido.");
 
         // Precio a Ethers
         uint256 _precioNuevaSubasta = _precioSubasta * 1 ether;
 
         // Nueva Subasta.
         subasta memory _nuevaSubasta;
-        _nuevaSubasta = subasta(contadorSubastas, block.timestamp + 1 hours, 0, _precioNuevaSubasta, _descripcionSubasta, msg.sender, msg.sender, true);
+        _nuevaSubasta = subasta(contadorSubastas, _tiempoFinSubasta, 0, _precioNuevaSubasta, _descripcionSubasta, msg.sender, msg.sender, true);
         subastas[contadorSubastas] = _nuevaSubasta;
+        historialSubastas.push(contadorSubastas);
 
         // Trigger Event.
         emit eventoSubastaCreada(contadorSubastas, _precioNuevaSubasta, _descripcionSubasta, msg.sender);
@@ -87,6 +92,45 @@ contract subastas_a_ciegas {
     function conseguirPrecioSubasta(uint256 _idSubasta) public view returns(uint256) {
     
         return subastas[_idSubasta].precioActualSubasta > subastas[_idSubasta].precioInicialSubasta ? subastas[_idSubasta].precioActualSubasta : subastas[_idSubasta].precioInicialSubasta;
+
+    }
+
+    function devolverSubastas() public view returns (subasta[] memory) {
+        subasta[] memory _resultado = new subasta[](historialSubastas.length);
+
+        for(uint i = 0; i < historialSubastas.length; i++) {
+            _resultado[historialSubastas[i]] = subastas[historialSubastas[i]];
+        }
+
+        return _resultado;
+
+    }
+
+    function devolverSubastasActivas() public view returns(subasta[] memory) {
+
+        subasta[] memory _resultadoFuncion = devolverSubastas();
+
+        
+
+        uint _contadorResultados = 0;
+
+        for(uint i = 0; i < _resultadoFuncion.length; i++) {
+            if(_resultadoFuncion[i].estadoSubasta) {
+                _contadorResultados++;
+            }
+        }
+
+        subasta[] memory _resultado = new subasta[](_contadorResultados);
+
+        _contadorResultados = 0;
+        for(uint i = 0; i < _resultadoFuncion.length; i++) {
+            if(_resultadoFuncion[i].estadoSubasta) {
+                _resultado[_contadorResultados] = _resultadoFuncion[i];
+                _contadorResultados++;
+            }
+        }
+
+        return _resultado;
 
     }
 
